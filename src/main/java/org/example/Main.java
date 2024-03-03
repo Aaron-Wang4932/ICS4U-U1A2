a8cc887b05184b0721e74d5994571d91fd5fc7d6
Aaron Wang <aaronw4932@gmail.com>
2024-03-03 11:44:59 -0500

    donee

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
-import javax.tools.Tool;
 
 public class Main extends JFrame implements ActionListener, KeyListener {
 
